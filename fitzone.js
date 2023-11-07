d49f79fe77ffdba2edc20378848dc6b5c6f998d1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 13:30:24 -0300

    ac_ed_Pronta

--- fitzone.js
+++ fitzone.js
@@ -14,51 +14,54 @@ mongoose.connect("mongodb://127.0.0.1:27017/itensfit", {
 });
 
 const UsuarioSchema = new mongoose.Schema({
-  nome: { type: String },
-  email: { type: String, required: true },
-  endereco: { type: String },
-  numero: { type: Number },
-  cep: { type: String, required: true },
-  nascimento: { type: Date, required: true },
-});
-
-const ItensFitSchema = new mongoose.Schema({
-  descricao: { type: String },
-  marca: { type: String },
-  quantidade: { type: Number },
-  fabricacao: { type: Date, required: true },
+  nome: { type: String, required: true },
+  senha: { type: String, required: true },
 });
 
 const Usuario = mongoose.model("Usuario", UsuarioSchema);
-const ItensFit = mongoose.model("ItensFit", ItensFitSchema);
 
 app.post("/cadastrousuario", async (req, res) => {
-  const { nome, email, endereco, numero, cep, nascimento } = req.body;
-
-  if (!nome || !email || !endereco || !numero || !cep || !nascimento) {
-    return res.status(400).json({ error: "Preencha todos os campos!" });
-  }
-
-  const emailExiste = await Usuario.findOne({ email: email });
-  if (emailExiste) {
-    return res.status(400).json({ error: "O email informado já existe" });
-  }
-
-  const usuario = new Usuario({
-    nome,
-    email,
-    endereco,
-    numero,
-    cep,
-    nascimento,
-  });
-
   try {
+    const { nome, senha } = req.body;
+    const usuario = new Usuario({
+      nome: nome,
+      senha: senha,
+    });
+
     const newUsuario = await usuario.save();
-    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+    res.json({ error: null, msg: "Cadastro de usuário realizado com sucesso", UsuarioId: newUsuario._id });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Erro interno do servidor" });
+    res.status(500).json({ error: "Erro interno do servidor", details: error.message });
+  }
+});
+
+const ItensFitSchema = new mongoose.Schema({
+  id_produtoacademia: { type: Number, required: true },
+  descricao: { type: String },
+  marca: { type: String },
+  quantidade: { type: Number },
+  fabricacao: { type: String },
+});
+
+const Itens = mongoose.model("ItensFit", ItensFitSchema);
+
+app.post("/cadastroitensfit", async (req, res) => {
+  try {
+    const { id_produtoacademia, descricao, marca, quantidade, fabricacao } = req.body;
+    const itens = new Itens({
+      id_produtoacademia: id_produtoacademia,
+      descricao: descricao,
+      marca: marca,
+      quantidade: quantidade,
+      fabricacao: fabricacao,
+    });
+
+    const newItens = await itens.save();
+    res.json({ error: null, msg: "Cadastro de itens realizado com sucesso", ItensId: newItens._id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro interno do servidor", details: error.message });
   }
 });
 
